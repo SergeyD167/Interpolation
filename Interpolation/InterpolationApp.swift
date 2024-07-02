9a3b4d9f0e82190fcdc9e74052bb04d3ae231567
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-07-02 23:37:15 +0300

    Initial Commit

---
+++ Interpolation/InterpolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterpolationApp.swift
+//  Interpolation
+//
+//  Created by Сергей Дятлов on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct InterpolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
